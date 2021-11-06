611a3ef2c6e55081fb218a8e350b22ea7dcf8593
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-11-07 01:40:41 +0200

    Create Animal.cs

---
+++ Inheritance - Lab/03. Hierarchical Inheritance./Animal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Farm;
+
+namespace Farm
+{
+    public class Animal 
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
