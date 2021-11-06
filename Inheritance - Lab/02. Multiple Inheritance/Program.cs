91d472b76f7364fbc942f76cc971aab4e3f93eba
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-11-07 01:33:19 +0200

    Create Program.cs

---
+++ Inheritance - Lab/02. Multiple Inheritance/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using Farm;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+
+        }
+    }
+}
