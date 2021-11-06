b6cbf3552385f7ca73c0d8e827d658da537fa5cf
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-11-07 01:40:01 +0200

    Create Program.cs

---
+++ Inheritance - Lab/03. Hierarchical Inheritance./Program.cs
@@ -0,0 +1,22 @@
+using System;
+using Farm;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+
+
+
+        }
+    }
+}
