c44007f834c3c39f6da2a9c8a55bc85d22143e54
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-11-07 01:26:03 +0200

    Create Program.cs

---
+++ Inheritance - Lab/01. Single Inheritance/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using Farm;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+            //Animal frog = new Animal();
+            //frog.Eat();
+
+        }
+    }
+}
