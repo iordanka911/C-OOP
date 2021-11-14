e3cf61488ff067b1e04ff7fce513e4f75274e53e
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-11-14 23:35:35 +0200

    Create encapsulationText.cs

---
+++ Encapsulation/encapsulationText.cs
@@ -0,0 +1,11 @@
+Exception- program stop to work
+Encapsulation= скрива неща с цел да си защитим нещо в класа, защитава данните от намеса на въняния свят 
+константи 
+прайвит полетата с малки букви
+ctrl-f
+ctrl-shift-f
+modificator access -private 
+modificator-protected 
+modificator-public- 
+internal-is is not accesible for external libriries
+cobsole/class library- 
