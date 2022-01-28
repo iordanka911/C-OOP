e7e25aed0bfef9ab9ffca7e8a4e6a0bb866d89a1
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2022-01-28 14:37:28 +0200

    Add files via upload

---
+++ InheritanceExercises/05.Restaurant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters= 50;
+        private const decimal CofeePrice = 3.50m;
+        public Coffee(string name, decimal price, double caffeine) 
+            : base(name, CofeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
