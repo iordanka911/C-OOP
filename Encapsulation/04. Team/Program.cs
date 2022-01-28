50391ac998015b70700b6ccc8634f546232be358
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2022-01-28 23:49:36 +0200

    Add files via upload

---
+++ Encapsulation/04. Team/Program.cs
@@ -0,0 +1,53 @@
+﻿using PersonInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            var team = new Team("Softuni");
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                team.AddPlayer(person);
+            }
+
+           
+            if (team.FirstTeam.Count==1)
+            {
+                Console.WriteLine("First team has " + team.FirstTeam.Count + " player.");
+            }
+            else
+            {
+                Console.WriteLine("First team has " + team.FirstTeam.Count + " players.");
+            }
+
+            if (team.ReserveTeam.Count==1)
+            {
+                Console.WriteLine("Reserve team has " + team.ReserveTeam.Count + " player.");
+            }
+            else
+            {
+                Console.WriteLine("Reserve team has " + team.ReserveTeam.Count + " players.");
+            }
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+
+
+    }
+}
---
+++ Encapsulation/04. Team/Team.cs
@@ -0,0 +1,39 @@
+﻿using PersonsInfo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name => this.name;
+
+        public IReadOnlyList<Person> FirstTeam => firstTeam;
+
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam;
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age<40)
+            {
+                this.firstTeam.Add(player);
+            }
+            else
+            {
+                this.reserveTeam.Add(player);
+            }
+        }
+    }
+}
